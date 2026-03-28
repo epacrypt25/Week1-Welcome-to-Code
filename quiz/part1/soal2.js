60e2dca408755c7b0f360b1515209ec00619e54b
Epa Crypt <ekopurnama250498@gmail.com>
2026-03-28 22:04:12 +0700

    Update soal2.js

--- quiz/part1/soal2.js
+++ quiz/part1/soal2.js
@@ -24,6 +24,17 @@ let tahun = 1945; // assign nilai variabel tahun disini! (dengan angka antara 19
 
 switch (bulan) {
   case 1: bulan = "Januari"; break;
+  case 2: bulan = "Februari"; break;
+  case 3: bulan = "Maret"; break;
+  case 4: bulan = "April"; break;
+  case 5: bulan = "Mei"; break;
+  case 6: bulan = "Juni"; break;
+  case 7: bulan = "Juli"; break;
+  case 8: bulan = "Agustus"; break;
+  case 9: bulan = "September"; break;
+  case 10: bulan = "Oktober"; break;
+  case 11: bulan = "November"; break;
+  case 12: bulan = "Desember"; break;
 }
 
 console.log(hari + " " + bulan + " " + tahun);
